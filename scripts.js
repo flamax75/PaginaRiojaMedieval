675556244ed5b3931977bcb7baf6269038f54096
Flaviu Maxim <flaviomaxim@gmail.com>
2024-06-16 21:04:35 +0200

    giro vertical 10 imagenes tiempo ajustado

--- scripts.js
+++ scripts.js
@@ -1,15 +1,49 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const galleryItems = document.querySelectorAll(".gallery-item");
+    const galleryItem = document.querySelector(".gallery-item");
+    const images = JSON.parse(galleryItem.getAttribute("data-images"));
+    let currentIndex = 0;
+    let isRotating = false;
 
-    galleryItems.forEach(item => {
-        const frontImage = item.getAttribute("data-image-front");
-        const backImage = item.getAttribute("data-image-back");
+    function updateImage(direction) {
+        const nextIndex = direction === 'right' ? (currentIndex + 1) % images.length : (currentIndex - 1 + images.length) % images.length;
+        const rotationClass = direction === 'right' ? 'rotate-right' : 'rotate-left';
 
-        item.innerHTML = `
-            <div class="gallery-item-inner">
-                <div class="gallery-item-front" style="background-image: url('${frontImage}');"></div>
-                <div class="gallery-item-back" style="background-image: url('${backImage}');"></div>
-            </div>
+        const galleryItemInner = document.createElement('div');
+        galleryItemInner.classList.add('gallery-item-inner');
+        galleryItemInner.innerHTML = `
+            <div class="gallery-item-front" style="background-image: url('${images[currentIndex]}');"></div>
+            <div class="gallery-item-back" style="background-image: url('${images[nextIndex]}');"></div>
         `;
-    });
+
+        galleryItem.innerHTML = '';
+        galleryItem.appendChild(galleryItemInner);
+
+        // Force a reflow to restart the animation
+        void galleryItemInner.offsetWidth;
+        galleryItemInner.classList.add(rotationClass);
+
+        currentIndex = nextIndex;
+    }
+
+    function handleMouseOver(event) {
+        if (isRotating) return;
+        isRotating = true;
+        const boundingRect = galleryItem.getBoundingClientRect();
+        const x = event.clientX - boundingRect.left;
+
+        if (x < boundingRect.width / 2) {
+            updateImage('left');
+        } else {
+            updateImage('right');
+        }
+
+        setTimeout(() => {
+            isRotating = false;
+        }, 2000); // Changed duration to 2000ms to match the CSS transition duration
+    }
+
+    galleryItem.addEventListener("mouseover", handleMouseOver);
+
+    // Initialize the first image
+    updateImage('right');
 });
